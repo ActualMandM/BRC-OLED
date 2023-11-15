368e40da4d70e1a75705b8c098a81d292fbd6496
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-11-14 16:35:08 -0800

    Initial commit.

---
+++ BRC-OLED/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Reptile;
+using System.Reflection;
+using UnityEngine;
+
+namespace BRC_OLED
+{
+    [BepInPlugin("com.MandM.BRC-OLED", "BRC-OLED", "1.0.0")]
+    [BepInProcess("Bomb Rush Cyberfunk.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("MandM.BRC-OLED.Harmony");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(EffectsUI))]
+    public class EffectsUIPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Init")]
+        public static void Init_Prefix()
+        {
+            var niceBlack = typeof(EffectsUI).GetField("niceBlack", BindingFlags.Public | BindingFlags.Static);
+            niceBlack?.SetValue(EffectsUI.niceBlack, new Color(0f, 0f, 0f, 1f));
+
+            var niceClear = typeof(EffectsUI).GetField("niceClear", BindingFlags.Public | BindingFlags.Static);
+            niceClear?.SetValue(EffectsUI.niceClear, new Color(0f, 0f, 0f, 0f));
+        }
+    }
+}
